56d215823b0666f6f3328b511372bec4e4d1d23d
David Ilievski <ilievskidavid97@gmail.com>
2022-11-13 16:57:48 +0100

    change to server url

--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,3 @@
 //const SERVER_URL = 'http://localhost:8080';
-const SERVER_URL = 'http://theheadhunter-server.davidilievski.dev:8080';
+const SERVER_URL = 'https://theheadhunter-server.davidilievski.dev:8080';
 export { SERVER_URL };
